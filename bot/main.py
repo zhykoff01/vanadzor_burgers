4eb31d96899891fa836c3638c0863a3a0b016ecd
zhyk01 <zhykoff01@gmail.com>
2022-09-05 19:01:15 +0400

    Pipfile.lock update

--- bot/main.py
+++ bot/main.py
@@ -1,21 +1,21 @@
 import logging
 import os
+from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
+
 TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
-dp = Dispatcher(Bot)
+dp = Dispatcher(bot)
 
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
@@ -30,12 +30,12 @@ async def on_shutdown(dispatcher):
     await bot.delete_webhook()
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.answer("Hi!\nI'm Vanadzor BurgerBot!\nPowered by aiogram.")
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 
-if __name__ == '__main__':
+ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     start_webhook(
         dispatcher=dp,
