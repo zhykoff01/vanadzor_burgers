5f96976dfbd7e14db31da1e8fb128a837ebd75e4
zhyk01 <zhykoff01@gmail.com>
2022-09-22 21:39:51 +0400

    replacing local postgres with heroku

--- db/config.py
+++ db/config.py
@@ -1,4 +1,3 @@
-from databases import Database
+import os
 
-database = Database('postgres://kbyqdyjvlbgtby:4733ea467695edd4d3341d2e2761b6c761b52e593cd9d1c94b657b83ec62e040@ec2-52'
-                    '-210-97-223.eu-west-1.compute.amazonaws.com:5432/dfvgjiu109718d')
+DATABASE_URL = os.environ['DATABASE_URL']
