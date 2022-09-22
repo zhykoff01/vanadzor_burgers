a2b8f3dc08b5048b505b815ad4d9d96ff510a754
zhyk01 <zhykoff01@gmail.com>
2022-09-22 20:07:19 +0400

    fix import

--- db/repository.py
+++ db/repository.py
@@ -1,14 +1,13 @@
-from config import database
+import config
 
 
 class SqlRepository:
 
     async def is_user_exist(self, user_id):
-        await database.execute('''SELECT * FROM users WHERE user_id = %s''', [int(user_id)])
-        some_response = database.fetch_one()
+        await config.database.execute('''SELECT * FROM users WHERE user_id = %s''', [int(user_id)])
+        some_response = config.database.fetch_one()
         return len(some_response) > 0
 
     async def save_user(self, user_id, username):
-        await database.execute('''INSERT INTO users(user_id, username) values (%s, %s)''',
-                               [int(user_id), str(username)])
-
+        await config.database.execute('''INSERT INTO users(user_id, username) values (%s, %s)''',
+                                      [int(user_id), str(username)])
