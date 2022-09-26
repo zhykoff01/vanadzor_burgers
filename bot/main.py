d27b3211787ab8294cc07fdca9d48979dc617322
zhyk01 <zhykoff01@gmail.com>
2022-09-26 15:37:04 +0400

    start func changed

--- bot/main.py
+++ bot/main.py
@@ -19,16 +19,16 @@ async def on_shutdown(dispatcher):
 async def start(message: types.Message):
     if not sqlRepository.is_user_exist(message.from_user.id):
         sqlRepository.save_user(message.from_user.id, message.from_user.username, message.from_user.language_code)
-    if message.from_user.language_code == 'ru':
-        await message.answer(
-            f'Привет, {message.from_user.get_mention(as_html=True)}, у нас ты можешь заказать самые вкусные бургеры',
-            parse_mode=types.ParseMode.HTML,
-        )
-    elif message.from_user.language_code == 'en':
-        await message.answer(
-            f'Hello, {message.from_user.get_mention(as_html=True)}, here you can order the most delicious burgers',
-            parse_mode=types.ParseMode.HTML,
-        )
+        if message.from_user.language_code == 'ru':
+            await message.answer(
+                f'Привет, {message.from_user.get_mention(as_html=True)}, у нас ты можешь заказать самые вкусные бургеры',
+                parse_mode=types.ParseMode.HTML,
+            )
+        elif message.from_user.language_code == 'en':
+            await message.answer(
+                f'Hello, {message.from_user.get_mention(as_html=True)}, here you can order the most delicious burgers',
+                parse_mode=types.ParseMode.HTML,
+            )
 
 
 if __name__ == '__main__':
