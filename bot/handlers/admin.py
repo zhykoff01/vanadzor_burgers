4d01e9f4ba6cea964f48df8f920e235eba90ed71
zhyk01 <zhykoff01@gmail.com>
2022-09-27 22:10:02 +0400

    admin panel added

--- bot/handlers/admin.py
+++ bot/handlers/admin.py
@@ -11,6 +11,14 @@ class FSMAdmin(StatesGroup):
     price = State()
 
 
+async def cancel_handler(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await message.reply('OK')
+
+
 async def add(message: types.Message):
     await FSMAdmin.photo.set()
     await message.reply('Download photo')
@@ -44,19 +52,11 @@ async def load_price(message: types.Message, state: FSMContext):
     await state.finish()
 
 
-async def cancel_handler(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    if current_state is None:
-        return
-    await state.finish()
-    await message.reply('OK')
-
-
 def register_handler_admin(dp: Dispatcher):
+    dp.register_message_handler(cancel_handler, state='*', commands='Cancel')
+    dp.register_message_handler(cancel_handler, Text(equals='Cancel', ignore_case=True), state='*')
     dp.register_message_handler(add, commands='Add', state=None)
     dp.register_message_handler(load_photo, content_types='photo', state=FSMAdmin.photo)
     dp.register_message_handler(load_name, state=FSMAdmin.name)
     dp.register_message_handler(load_description, state=FSMAdmin.description)
     dp.register_message_handler(load_price, state=FSMAdmin.price)
-    dp.register_message_handler(cancel_handler, state='*', commands='Cancel')
-    dp.register_message_handler(cancel_handler, Text(equals='Cancel', ignore_case=True), state='*')
