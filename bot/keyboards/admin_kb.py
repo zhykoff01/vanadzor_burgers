aac959ab3904ab407d0dff4cd654f47e6bfc0c2c
zhyk01 <zhykoff01@gmail.com>
2022-09-28 00:04:24 +0400

    admin keyboard added

--- bot/keyboards/admin_kb.py
+++ bot/keyboards/admin_kb.py
@@ -3,5 +3,6 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 button_load = KeyboardButton('/Add')
 button_delete = KeyboardButton('/Delete')
+button_cancel = KeyboardButton('/Cancel')
 
-button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete)
+button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load, button_delete, button_cancel)
