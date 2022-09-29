0eff254be0b9d464cdc90c81379a02dd9e5d956e
zhyk01 <zhykoff01@gmail.com>
2022-09-29 13:43:06 +0400

    Menu added

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -18,15 +18,15 @@ async def start_command(message: types.Message):
     btn3_ru = types.KeyboardButton('Информация')
     markup_en.add(btn1_en, btn2_en, btn3_en)
     markup_ru.add(btn1_ru, btn2_ru, btn3_ru)
-    if not sqlRepository.is_user_exist(message.from_user.id):
+    if not await sqlRepository.is_user_exist(message.from_user.id):
         await sqlRepository.save_user(message.from_user.id, message.from_user.username, message.from_user.language_code)
-    if sqlRepository.user_language_code(message.from_user.id) == 'ru':
+    if await sqlRepository.user_language_code(message.from_user.id) == 'ru':
         await message.answer(
             f'Привет, {message.from_user.get_mention(as_html=True)}, у нас ты можешь заказать самые вкусные бургеры',
             parse_mode=types.ParseMode.HTML,
             reply_markup=markup_ru,
         )
-    elif sqlRepository.user_language_code(message.from_user.id) == 'en':
+    elif await sqlRepository.user_language_code(message.from_user.id) == 'en':
         await message.answer(
             f'Hello, {message.from_user.get_mention(as_html=True)}, here you can order the most delicious burgers',
             parse_mode=types.ParseMode.HTML,
