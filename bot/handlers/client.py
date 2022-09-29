7e09c483a5a64c6e9bf714af9ba71bd93f5db6ef
zhyk01 <zhykoff01@gmail.com>
2022-09-29 12:36:16 +0400

    Main menu added

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -9,24 +9,29 @@ sqlRepository = SqlRepository()
 
 
 async def start_command(message: types.Message):
+    markup_en, markup_ru = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1_en = types.KeyboardButton('Make order')
+    btn2_en = types.KeyboardButton('Geo')
+    btn3_en = types.KeyboardButton('Info')
+    btn1_ru = types.KeyboardButton('Сделать заказ')
+    btn2_ru = types.KeyboardButton('Расположение')
+    btn3_ru = types.KeyboardButton('Информация')
+    markup_en.add(btn1_en, btn2_en, btn3_en)
+    markup_ru.add(btn1_ru, btn2_ru, btn3_ru)
     if not sqlRepository.is_user_exist(message.from_user.id):
         await sqlRepository.save_user(message.from_user.id, message.from_user.username, message.from_user.language_code)
     if message.from_user.language_code == 'ru':
         await message.answer(
             f'Привет, {message.from_user.get_mention(as_html=True)}, у нас ты можешь заказать самые вкусные бургеры',
             parse_mode=types.ParseMode.HTML,
+            reply_markup=markup_ru
         )
     elif message.from_user.language_code == 'en':
         await message.answer(
             f'Hello, {message.from_user.get_mention(as_html=True)}, here you can order the most delicious burgers',
             parse_mode=types.ParseMode.HTML,
+            reply_markup=markup_en
         )
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton('Make order')
-    btn2 = types.KeyboardButton('Geo')
-    btn3 = types.KeyboardButton('Info')
-    markup.add(btn1, btn2, btn3)
-    await bot.send_message(message.chat.id, text='Main menu', reply_markup=markup)
 
 
 async def help_command(message: types.Message):
