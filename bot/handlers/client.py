10bd027b95d277a5f327e678970dfb4d90ded6e0
zhyk01 <zhykoff01@gmail.com>
2022-09-29 14:14:47 +0400

    Menu added

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -20,13 +20,13 @@ async def start_command(message: types.Message):
     markup_ru.add(btn1_ru, btn2_ru, btn3_ru)
     if not sqlRepository.is_user_exist(message.from_user.id):
         await sqlRepository.save_user(message.from_user.id, message.from_user.username, message.from_user.language_code)
-    if message.from_user.id == 'ru':
+    if message.from_user.language_code == 'ru':
         await message.answer(
             f'Привет, {message.from_user.get_mention(as_html=True)}, у нас ты можешь заказать самые вкусные бургеры',
             parse_mode=types.ParseMode.HTML,
             reply_markup=markup_ru,
         )
-    elif message.from_user.id == 'en':
+    elif message.from_user.language_code == 'en':
         await message.answer(
             f'Hello, {message.from_user.get_mention(as_html=True)}, here you can order the most delicious burgers',
             parse_mode=types.ParseMode.HTML,
