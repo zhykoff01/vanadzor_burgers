9ec78a93992edc3cc2aaccf61d14936607fcae7d
zhyk01 <zhykoff01@gmail.com>
2022-10-03 10:35:04 +0400

    Menu added

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -1,36 +1,16 @@
 import types
 from aiogram import types, Dispatcher
 from db.repository import SqlRepository
+from bot.keyboards.client_kb import KeyboardClient
 
 
 sqlRepository = SqlRepository()
 
 
 async def start_command(message: types.Message):
-    markup_en = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup_ru = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1_en = types.KeyboardButton('Make order')
-    btn2_en = types.KeyboardButton('Geo')
-    btn3_en = types.KeyboardButton('Info')
-    btn1_ru = types.KeyboardButton('Сделать заказ')
-    btn2_ru = types.KeyboardButton('Расположение')
-    btn3_ru = types.KeyboardButton('Информация')
-    markup_en.add(btn1_en, btn2_en, btn3_en)
-    markup_ru.add(btn1_ru, btn2_ru, btn3_ru)
+    await KeyboardClient.main_menu(message.text)
     if not await sqlRepository.is_user_exist(message.from_user.id):
         await sqlRepository.save_user(message.from_user.id, message.from_user.username, message.from_user.language_code)
-    if message.from_user.language_code == 'ru':
-        await message.answer(
-            f'Привет, {message.from_user.get_mention(as_html=True)}, у нас ты можешь заказать самые вкусные бургеры',
-            parse_mode=types.ParseMode.HTML,
-            reply_markup=markup_ru,
-        )
-    else:
-        await message.answer(
-            f'Hello, {message.from_user.get_mention(as_html=True)}, here you can order the most delicious burgers',
-            parse_mode=types.ParseMode.HTML,
-            reply_markup=markup_en,
-        )
 
 
 async def help_command(message: types.Message):
@@ -89,12 +69,6 @@ async def send_menu(message: types.Message):
         dishes[1], f'Title: {dishes[2]}\nDescription: {dishes[4]}\nPrice: {dishes[5]}',
         reply_markup=markup_en,
     )
-
-
-# async def send_menu(message: types.Message):
-#     some_response = await sqlRepository.extract_menu(message)
-#     for res in some_response:
-#         await bot.send_photo(message.from_user.id, res[1], f'{res[2]}\nDescription: {res[3]}\nPrice: {res[-1]}')
 
 
 def register_handler_client(dp: Dispatcher):
---
+++ bot/keyboards/client_kb.py
@@ -0,0 +1,33 @@
+from aiogram import types
+
+
+class KeyboardClient:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def main_menu(self, message):
+        markup_en = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup_ru = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1_en = types.KeyboardButton('Make order')
+        btn2_en = types.KeyboardButton('Geo')
+        btn3_en = types.KeyboardButton('Info')
+        btn1_ru = types.KeyboardButton('Сделать заказ')
+        btn2_ru = types.KeyboardButton('Расположение')
+        btn3_ru = types.KeyboardButton('Информация')
+        markup_en.add(btn1_en, btn2_en, btn3_en)
+        markup_ru.add(btn1_ru, btn2_ru, btn3_ru)
+        if message.from_user.language_code == 'ru':
+            await message.answer(
+                f'Привет, {message.from_user.get_mention(as_html=True)}, '
+                f'у нас ты можешь заказать самые вкусные бургеры',
+                parse_mode=types.ParseMode.HTML,
+                reply_markup=markup_ru,
+            )
+        else:
+            await message.answer(
+                f'Hello, {message.from_user.get_mention(as_html=True)}, '
+                f'here you can order the most delicious burgers',
+                parse_mode=types.ParseMode.HTML,
+                reply_markup=markup_en,
+            )
