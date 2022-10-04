a363885dac61b9d626751d2cd4fbab313b175448
zhyk01 <zhykoff01@gmail.com>
2022-10-04 15:36:53 +0400

    FSMClient added

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -1,7 +1,6 @@
 import types
 from aiogram import types, Dispatcher
 from aiogram.dispatcher import FSMContext
-from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from db.repository import SqlRepository
 from bot.keyboards.client_kb import KeyboardClient
@@ -17,14 +16,7 @@ class ClientHandlers:
     sqlRepository = SqlRepository()
     keyboardClient = KeyboardClient()
 
-    async def rollback_handler(self, state: FSMContext):
-        current_state = await state.get_state()
-        if current_state is None:
-            return
-        await FSMClient.previous()
-
     async def start_command(self, message: types.Message, state: FSMContext):
-        await state.update_data(action=message.text)
         if await self.sqlRepository.user_language_code(message.from_user.id) == 'ru':
             await message.answer(
                 f'Привет, {message.from_user.get_mention(as_html=True)}, '
@@ -42,7 +34,6 @@ class ClientHandlers:
         if not await self.sqlRepository.is_user_exist(message.from_user.id):
             await self.sqlRepository.save_user(message.from_user.id, message.from_user.username,
                                                message.from_user.language_code)
-        await FSMClient.menu.set()
 
     async def help_command(self, message: types.Message):
         if await self.sqlRepository.user_language_code(message.from_user.id) == 'ru':
@@ -54,8 +45,7 @@ class ClientHandlers:
                 f'Hello, here you can order the most delicious burgers in Vanadzor'
             )
 
-    async def menu(self, message: types.Message, state: FSMContext):
-        await state.update_data(category=message.text)
+    async def menu(self, message: types.Message):
         if await self.sqlRepository.user_language_code(message.from_user.id) == 'ru':
             await message.answer(
                 f'Выбери категорию',
@@ -66,56 +56,37 @@ class ClientHandlers:
                 f'Choose a category',
                 reply_markup=await self.keyboardClient.menu_en(),
             )
-        await FSMClient.next()
 
-    async def burgers(self, message: types.Message, state: FSMContext):
-        await state.update_data(subcategory=message.text)
+    async def burgers(self, message: types.Message):
         markup = await self.keyboardClient.burgers()
         await message.answer(
             f'Choose a burger',
             reply_markup=markup,
         )
-        await FSMClient.next()
 
-    async def pizza(self, message: types.Message, state: FSMContext):
-        await state.update_data(subcategory=message.text)
+    async def pizza(self, message: types.Message):
         markup = await self.keyboardClient.pizza()
         await message.answer(
             f'Choose a pizza',
             reply_markup=markup,
         )
-        await FSMClient.next()
 
-    async def drinks(self, message: types.Message, state: FSMContext):
-        await state.update_data(subcategory=message.text)
+    async def drinks(self, message: types.Message):
         markup = await self.keyboardClient.drinks()
         await message.answer(
             f'Choose a drink',
             reply_markup=markup,
         )
-        await FSMClient.next()
 
-    async def send_menu(self, message: types.Message, state: FSMContext):
-        await state.update_data(dish=message.text)
+    async def send_menu(self, message: types.Message):
         dishes = await self.sqlRepository.extract_menu(message.text)
         markup = await self.keyboardClient.send_menu()
         await message.answer_photo(
             dishes[1], f'Title: {dishes[2]}\nDescription: {dishes[4]}\nPrice: {dishes[5]}',
             reply_markup=markup,
         )
-        await state.finish()
 
     def register_handler_client(self, dp: Dispatcher):
-        dp.register_message_handler(
-            self.rollback_handler,
-            lambda message: 'Back'.__contains__(message.text),
-            state='*',
-        )
-        dp.register_message_handler(
-            self.rollback_handler,
-            Text(equals='back', ignore_case=True),
-            state='*',
-        )
         dp.register_message_handler(
             self.start_command,
             commands=['start'],
