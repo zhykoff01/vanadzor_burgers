be3d2c96bb551919dc6a5919873d48ddcae7ec1d
zhyk01 <zhykoff01@gmail.com>
2022-10-05 17:57:51 +0400

    FSMClient added

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -7,16 +7,18 @@ from bot.keyboards.client_kb import KeyboardClient
 
 
 class FSMClient(StatesGroup):
-    menu = State()
-    dish = State()
-    send_menu = State()
+    one = State()
+    two = State()
+    three = State()
+    four = State()
+    five = State()
 
 
 class ClientHandlers:
     sqlRepository = SqlRepository()
     keyboardClient = KeyboardClient()
 
-    async def start_command(self, message: types.Message, state: FSMContext):
+    async def start_command(self, state: FSMContext, message: types.Message):
         if await self.sqlRepository.user_language_code(message.from_user.id) == 'ru':
             await message.answer(
                 f'Привет, {message.from_user.get_mention(as_html=True)}, '
@@ -34,18 +36,10 @@ class ClientHandlers:
         if not await self.sqlRepository.is_user_exist(message.from_user.id):
             await self.sqlRepository.save_user(message.from_user.id, message.from_user.username,
                                                message.from_user.language_code)
+        await state.update_data(one=message.text)
+        await FSMClient.one.set()
 
-    async def help_command(self, message: types.Message):
-        if await self.sqlRepository.user_language_code(message.from_user.id) == 'ru':
-            await message.answer(
-                f'Привет, в этом боте ты можешь заказать самые вкусные бургеры в Ванадзоре'
-            )
-        else:
-            await message.answer(
-                f'Hello, here you can order the most delicious burgers in Vanadzor'
-            )
-
-    async def menu(self, message: types.Message):
+    async def menu(self, state: FSMContext, message: types.Message):
         if await self.sqlRepository.user_language_code(message.from_user.id) == 'ru':
             await message.answer(
                 f'Выбери категорию',
@@ -56,62 +50,74 @@ class ClientHandlers:
                 f'Choose a category',
                 reply_markup=await self.keyboardClient.menu_en(),
             )
+        await state.update_data(two=message.text)
+        await FSMClient.next()
 
-    async def burgers(self, message: types.Message):
+    async def burgers(self, state: FSMContext, message: types.Message):
         markup = await self.keyboardClient.burgers()
         await message.answer(
             f'Choose a burger',
             reply_markup=markup,
         )
+        await state.update_data(three=message.text)
+        await FSMClient.next()
 
-    async def pizza(self, message: types.Message):
+    async def pizza(self, state: FSMContext, message: types.Message):
         markup = await self.keyboardClient.pizza()
         await message.answer(
             f'Choose a pizza',
             reply_markup=markup,
         )
+        await state.update_data(three=message.text)
+        await FSMClient.next()
 
-    async def drinks(self, message: types.Message):
+    async def drinks(self, state: FSMContext, message: types.Message):
         markup = await self.keyboardClient.drinks()
         await message.answer(
             f'Choose a drink',
             reply_markup=markup,
         )
+        await state.update_data(three=message.text)
+        await FSMClient.next()
 
-    async def send_menu(self, message: types.Message):
+    async def send_menu(self, state: FSMContext, message: types.Message):
         dishes = await self.sqlRepository.extract_menu(message.text)
         markup = await self.keyboardClient.send_menu()
         await message.answer_photo(
             dishes[1], f'Title: {dishes[2]}\nDescription: {dishes[4]}\nPrice: {dishes[5]}',
             reply_markup=markup,
         )
+        await state.update_data(four=message.text)
+        await FSMClient.next()
 
     def register_handler_client(self, dp: Dispatcher):
         dp.register_message_handler(
             self.start_command,
-            commands=['start'],
-        )
-        dp.register_message_handler(
-            self.help_command,
-            commands=['help'],
+            commands=['start', 'help'],
+            state=None,
         )
         dp.register_message_handler(
             self.menu,
             lambda message: ('Make order', 'Сделать заказ').__contains__(message.text),
+            state='one',
         )
         dp.register_message_handler(
             self.burgers,
             lambda message: ('Burgers', 'Бургеры').__contains__(message.text),
+            state='two',
         )
         dp.register_message_handler(
             self.pizza,
             lambda message: 'Pizza'.__contains__(message.text),
+            state='three',
         )
         dp.register_message_handler(
             self.burgers,
             lambda message: 'Drink'.__contains__(message.text),
+            state='four',
         )
         dp.register_message_handler(
             self.send_menu,
             lambda message: ('Cheeseburger', 'Chickenburger', 'Bigmac').__contains__(message.text),
+            state='five',
         )
