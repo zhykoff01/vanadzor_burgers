b92c6eb8cf32688fd7a01fc5da27c5c481183b9b
zhyk01 <zhykoff01@gmail.com>
2022-10-09 12:38:41 +0400

    FSMClient added

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -108,7 +108,8 @@ class ClientHandlers:
     def register_handler_client(self, dp: Dispatcher):
         dp.register_message_handler(
             self.start_command,
-            lambda message: ('start', 'help').__contains__(message.text),
+            lambda message: 'Main menu'.__contains__(message.text),
+            commands=('start', 'help'),
             state='*',
         )
         dp.register_message_handler(
